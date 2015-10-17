8c9eae9d70a8f798d68bc888ed0d4db62b3b613b
satej1210 <satej1210@live.com>
2015-10-16 21:10:31 -0500

    Update

--- ICSGenerator.py
+++ ICSGenerator.py
@@ -1,8 +1,8 @@
 import sys
 def getData():
-    inp=input("Enter event title : ")
+    inp=input("Enter event title(type ENDE to end entering events): ")
     if inp=="ENDE":
-        return
+        return "END"
     else:
         f=""
         f+=('BEGIN:VEVENT\nSUMMARY:'+inp+"\n")
@@ -18,17 +18,34 @@ def getData():
 def process():
     k=input("Enter number of events : ")
     l=input("Enter filename : ")
-    for i in range(0,int(k)):
+    f = open(l+'.ics', 'ab')
+    f.write(bytes("BEGIN:VCALENDAR\n", 'UTF-8'))
+    f.close()
+    i=0
+    while i < int(k):
         string = getData()
+        if string=="END":
+            print("Ending...")
+            break
         m = input("Is this event alright?(Y/N) : ")
         if m=='Y':
-            f = open(l+'.ics', 'wb')
-            print("Writing " + string + "...\n")
+            f = open(l+'.ics', 'ba')
+            print("Writing event to ICS...\n")
             f.write(bytes(string, 'UTF-8'))
             f.close()
         else:
             print("Rerunning the event maker...\n")
-def menu():
-    
+            i=-1
+    f = open(l+'.ics', 'ab')
+    f.write(bytes("END:VCALENDAR", 'UTF-8'))
+    f.close()
+    print("Your file has been saved as " + l + ".ics")
 if __name__ == "__main__":
-    menu()
+    if len(sys.argv)==2:
+        if sys.argv[-4:len(sys.argv[0])]==".csv":
+            print("Woo! A CSV!\n")
+        else:
+            print("Please gimma a CSV file(yes, with the extension '.csv')\n")
+    else:
+        print("I see you wanna type in the events with this program... Let's start!\n")
+        process()
